65496098dbe47257adcd4eec7526a0482a446795
RobeSantoro <santoro.robe@gmail.com>
2022-01-10 15:24:30 +0100

    Refactoring Render Function, Added glsl vert and frag shaders

--- src/script.js
+++ src/script.js
@@ -6,7 +6,7 @@ import { MeshSurfaceSampler } from 'three/examples/jsm/math/MeshSurfaceSampler.j
 import Stats from 'three/examples/jsm/libs/stats.module.js'
 
 // Sizes
- const sizes = {
+const sizes = {
   width: window.innerWidth,
   height: window.innerHeight
 }
@@ -29,99 +29,12 @@ controls.dampingFactor = 0.25
 controls.autoRotate = true
 controls.autoRotateSpeed = 2
 
-//************************************ 
-// Create a Geometry Buffer and Points 
-//************************************ 
+// Stats
+const stats = new Stats()
+stats.showPanel(0)
+document.body.appendChild(stats.dom)
 
-const group = new THREE.Group();
-scene.add(group);
-
-// The geometry of the points 
-const sparklesGeometry = new THREE.BufferGeometry()
-// The material of the points 
-const sparklesMaterial = new THREE.PointsMaterial({
-  size: .1,
-  alphaTest: 0.2,
-  map: new THREE.TextureLoader().load('./assets/textures/particle.png'),
-  vertexColors: true // Let Three.js knows that each point has a different color
-});
-
-// Create a Points object 
-const points = new THREE.Points(sparklesGeometry, sparklesMaterial)
-
-// Add the points into the scene 
-group.add(points)
-
-
-//******************************
-// Create the AddPoint function
-//******************************
-
-// Vector to sample a random point 
-const tempPosition = new THREE.Vector3();
-// Define the colors we want 
-const palette = [new THREE.Color("#FEDD23"), new THREE.Color("#F24361"), new THREE.Color("#99248F"), new THREE.Color("#662D91")];
-
-// Used to store each particle coordinates & color 
-const vertices = []
-const colors = []
-
-function addPoint() {
-  // Sample a new point 
-  sampler.sample(tempPosition);
-  // Push the point coordinates 
-  vertices.push(tempPosition.x, tempPosition.y, tempPosition.z);
-  // Update the position attribute with the new coordinates 
-  sparklesGeometry.setAttribute("position", new THREE.Float32BufferAttribute(vertices, 3)  );
-  
-  // Get a random color from the palette 
-  const color = palette[Math.floor(Math.random() * palette.length)];
-  // Push the picked color 
-  colors.push(color.r, color.g, color.b);
-  // Update the color attribute with the new colors 
-  sparklesGeometry.setAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
-}
-
-//******************* 
-// Logo Mesh Sampling 
-//******************* 
-
-let logo = null
-let sampler = null
-
-// Add GLTF Object
-new GLTFLoader().load(
-  './assets/LogoMergedClean.glb', ( gltf ) => {
-
-    // Get the buffered geometry of rhe faces
-    logo = gltf.scene.children[0].children[1]
-
-    logo.material = new THREE.MeshBasicMaterial({
-      wireframe: true,
-      color: 0x505050,
-      transparent: true,
-      opacity: 0.05,      
-      depthWrite: false      
-    })
-
-    // Create a surface sampler from the loaded model 
-    sampler = new MeshSurfaceSampler(logo).build()
-
-    // Start the rendering loop 
-    renderer.setAnimationLoop(tick()) // Not sure what this does
-    
-    scene.add(gltf.scene)
-
-  },
-  undefined,
-  (xhr) => { console.log((xhr.loaded / xhr.total * 100) + '% loaded') },
-  (error) => { console.log(error) }
-)
-
-
-
-
-// Renderer 
+// Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
   antialias: true,
@@ -130,53 +43,152 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.outputEncoding = THREE.sRGBEncoding;
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 1;
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.toneMapping = THREE.ACESFilmicToneMapping
+renderer.toneMappingExposure = 1
 renderer.setClearColor(0x101010, 0)
 
-// Stats
-const stats = new Stats()
-stats.showPanel(0)
-document.body.appendChild(stats.dom)
+const group = new THREE.Group()
+scene.add(group)
 
-// Animate
-const clock = new THREE.Clock()
-let lastElapsedTime = 0
-let FPS = 0
+const sparkles = []
+window.sparkles = sparkles
 
-const tick = () =>
-{
-  const elapsedTime = clock.getElapsedTime()
-  const deltaTime = elapsedTime - lastElapsedTime
-  lastElapsedTime = elapsedTime
+const sparklesGeometry = new THREE.BufferGeometry()
+const sparklesMaterial = new THREE.ShaderMaterial({
+  uniforms: {
+    pointTexture: {
+      value: new THREE.TextureLoader().load('./assets/textures/particle.png')
+    }
+  },
+  vertexShader: document.getElementById("vertexshader").textContent,
+  fragmentShader: document.getElementById("fragmentshader").textContent,
+  depthTest: true,
+  depthWrite: true,
+  blending: THREE.AdditiveBlending
+})
 
-  //FPS
-  FPS = Math.round(1/deltaTime)
+const points = new THREE.Points(sparklesGeometry, sparklesMaterial)
+group.add(points)
 
-  if (vertices.length < 10000) {
-    addPoint()
-    addPoint()    
+const p1 = new THREE.Vector3()
+let sampler = null
+const lines = []
+let linesColors = [new THREE.Color(0xFAAD80).multiplyScalar(0.5),
+                  new THREE.Color(0xFF6767).multiplyScalar(0.5),
+                  new THREE.Color(0xFF3D68).multiplyScalar(0.5),
+                  new THREE.Color(0xA73489).multiplyScalar(0.5)]
+
+
+function initLines() {
+  sampler = new MeshSurfaceSampler(logoFaces).build()
+
+  for (let i = 0; i < 6; i++) {
+    sampler.sample(p1)
+    const linesMesh = {
+      colorIndex: i % 4,
+      previous: p1.clone()
+    }
+    lines.push(linesMesh)
   }
 
-  // Update stats
-  stats.update()
- 
-  // Update controls
-  controls.update()
-
-  // Render
-  renderer.render(scene, camera)  
-
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick)
+  renderer.setAnimationLoop(render)
 }
 
-tick()
+let logoFaces = null
+let logoSides = null
 
+new GLTFLoader().load(
+  "./assets/LogoMergedClean.glb",
+  (gltf) => {
+    logoFaces = gltf.scene.children[0].children[1]
+    logoSides = gltf.scene.children[0].children[0]
+    initLines()
+    scene.add(logoSides)
+  },
+  (xhr) => //console.log((xhr.loaded / xhr.total) * 100 + "% loaded")
+    null,
+  (err) => console.error(err)
+)
 
-window.addEventListener('resize', () =>
-{
+const tempSparklesArrayColors = []
+function findNextVector(line) {
+  let ok = false
+  while (!ok) {
+    sampler.sample(p1)
+
+    if (p1.distanceTo(line.previous) < 2) {
+      line.previous = p1.clone()
+
+      const spark = new Sparkle()
+      spark.setup(line.previous)
+      sparkles.push(spark)
+
+      tempSparklesArrayColors.push(linesColors[line.colorIndex].r, linesColors[line.colorIndex].g, linesColors[line.colorIndex].b)
+      sparklesGeometry.setAttribute("color", new THREE.Float32BufferAttribute(tempSparklesArrayColors, 3))
+
+      ok = true
+    }
+  }
+}
+
+class Sparkle extends THREE.Vector3 {
+  setup(origin) {
+    this.add(origin).multiplyScalar(2)
+    this.dest = origin
+
+    this._size = Math.random() * 5 + 0.5
+    this.size = 1
+    this.scaleSpeed = Math.random() * 0.03 + 0.03
+    this.stop = false
+  }
+  update() {
+    this.x += (this.dest.x - this.x) * 0.01
+    this.y += (this.dest.y - this.y) * 0.01
+    this.z += (this.dest.z - this.z) * 0.01
+    if (this.size < this._size) {
+      this.size += this.scaleSpeed
+    } else {
+      /* if (this.distanceTo(this.dest) < 0.1) {
+        this.stop = true
+      } */
+    }
+  }
+}
+
+let tempSparklesArray = []
+let tempSparklesArraySizes = []
+
+// Render
+function render(a) {
+
+  if (sparkles.length < 30000) {
+    lines.forEach(l => {
+      findNextVector(l)
+      findNextVector(l)
+      findNextVector(l)
+    })
+  }
+
+  sparkles.forEach((s, i) => {
+    if (!s.stop) {
+      s.update()
+    }
+    tempSparklesArray[(i * 3)] = s.x
+    tempSparklesArray[(i * 3) + 1] = s.y
+    tempSparklesArray[(i * 3) + 2] = s.z
+    tempSparklesArraySizes[i] = s.size
+  })
+  sparklesGeometry.setAttribute("position", new THREE.Float32BufferAttribute(tempSparklesArray, 3))
+  sparklesGeometry.setAttribute("size", new THREE.Float32BufferAttribute(tempSparklesArraySizes, 1))
+
+  // Render
+  renderer.render(scene, camera)
+  // Stats
+  stats.update()
+}
+
+window.addEventListener('resize', () => {
   // Update sizes
   sizes.width = window.innerWidth
   sizes.height = window.innerHeight
